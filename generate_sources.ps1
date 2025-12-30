f3df9bc9f92f8b7fe1e23ceba3d2acb2d7bcbe29
Jakub Janzo <janzo.fx@gmail.com>
2025-12-30 16:00:49 +0100

    chore: Remove build artifact files from version control and add to gitignore

--- generate_sources.ps1
+++
@@ -1,2 +0,0 @@
-$files = Get-ChildItem -Path "src\main\java" -Filter "*.java" -Recurse
-$files | ForEach-Object { "`"$($_.FullName.Replace('\', '/'))`"" } | Out-File -Encoding ASCII sources_quoted.txt
